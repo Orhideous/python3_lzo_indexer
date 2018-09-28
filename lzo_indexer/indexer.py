a5c4cbd5f02763af6f0e6dacca4684505b57d1dc
Andriy Kushnir <a.kushnir@smartweb.com.ua>
2018-09-28 19:53:33 +0300

    Refresh codebase to python3.6+

--- lzo_indexer/indexer.py
+++ lzo_indexer/indexer.py
@@ -1,16 +1,24 @@
 import struct
-from collections import namedtuple
-from StringIO import StringIO
+from typing import BinaryIO, NamedTuple
 
 # Magic string expected at the start of the file to verify it's LZO
-_LZO_MAGIC = bytearray("\x89LZO\x00\r\n\x1a\n")
+LZO_MAGIC = bytearray([0x89, 0x4c, 0x5a, 0x4f, 0x00, 0x0d, 0x0a, 0x1a, 0x0a])
+SUFFIXES = frozenset({".lzo", ".lzop"})
 
 _COMPRESSION_CHECKSUMS = (0x02, 0x200)  # ADLER32 CRC32
 _DECOMPRESSION_CHECKSUMS = (0x01, 0x100)  # ADLER32 CRC32
 
+F_H_EXTRA_FIELD = 0x00000040
 
-def _parse_header(lzo_file):
-    """Parse and verify the header of an LZO file, returning a tuple
+
+class Checksums(NamedTuple):
+    compressed: int
+    decompressed: int
+
+
+def _parse_header(lzo_file: BinaryIO) -> Checksums:
+    """
+    Parse and verify the header of an LZO file, returning a tuple
     of the number of compressed/decompressed checksums expected at the
     end of each block.
     """
@@ -19,17 +27,16 @@ def _parse_header(lzo_file):
         raise Exception("File object must be at offset 0")
 
     # Parse the header
-    if lzo_file.read(9) != _LZO_MAGIC:
+    if lzo_file.read(9) != LZO_MAGIC:
         raise Exception("Invalid lzo file")
 
     # Ignore a bunch of values from the header
     # TODO: We should validate these
-    lzop_version = lzo_file.read(2)
-    library_version = lzo_file.read(2)
-    extract_version = lzo_file.read(2)
-
-    method = lzo_file.read(1)
-    level = lzo_file.read(1)
+    lzo_file.read(2)  # lzop_version
+    lzo_file.read(2)  # library_version
+    lzo_file.read(2)  # extract_version
+    lzo_file.read(1)  # method
+    lzo_file.read(1)  # level
 
     # Checksum flags
     flags, = struct.unpack(">I", lzo_file.read(4))
@@ -45,35 +52,39 @@ def _parse_header(lzo_file):
             num_decompressed_checksums += 1
 
     # Parse out the mode/mtime/gmtdiff values we're not interested in
-    mode = lzo_file.read(4)
-    mtime = lzo_file.read(4)
-    gmtdiff = lzo_file.read(4)
+    lzo_file.read(4)  # mode
+    lzo_file.read(4)  # mtime
+    lzo_file.read(4)  # gmtdiff
 
     # Extract the filename
     filename_length = ord(lzo_file.read(1))
     if filename_length > 0:
-        filename = str(lzo_file.read(filename_length))
+        str(lzo_file.read(filename_length))  # filename
 
     # TODO: Verify the header checksum against these bytes
     lzo_file.read(4)
 
     # Process extra header field for lzo < 1.08. This is a checksum that
     # needs to also be validated
-    if (flags & 0x00000040) != 0:
+    if (flags & F_H_EXTRA_FIELD) != 0:
         size, = struct.unpack(">I", lzo_file.read(4))
         if size > 0:
             lzo_file.read(size)
         lzo_file.read(4)
 
-    return num_compressed_checksums, num_decompressed_checksums
+    return Checksums(
+        compressed=num_compressed_checksums,
+        decompressed=num_decompressed_checksums
+    )
 
 
-def get_lzo_blocks(lzo_file):
-    """Return a generator containing all of the block offsets for each
+def get_lzo_blocks(lzo_file: BinaryIO):
+    """
+    Return a generator containing all of the block offsets for each
     compressed block of data in the LZO file.
     """
 
-    num_compressed_chksms, num_decompressed_chksms = _parse_header(lzo_file)
+    checksums = _parse_header(lzo_file)
 
     while True:
         decompressed_blocksize, = struct.unpack(">I", lzo_file.read(4))
@@ -82,11 +93,11 @@ def get_lzo_blocks(lzo_file):
 
         compressed_blocksize, = struct.unpack(">I", lzo_file.read(4))
 
-        num_chksms_to_skip = num_decompressed_chksms
+        checksums_to_skip = checksums.decompressed
         if decompressed_blocksize == compressed_blocksize:
-            num_chksms_to_skip += num_compressed_chksms
+            checksums_to_skip += checksums.compressed
 
-        skip = 4 * num_chksms_to_skip
+        skip = 4 * checksums_to_skip
 
         position = lzo_file.tell()
 
@@ -98,22 +109,9 @@ def get_lzo_blocks(lzo_file):
         lzo_file.seek(next_block)  # Seek to the next block
 
 
-def index_lzo_string(string):
-    """Return a generator containing block offsets for each compressed block
-    of data in the LZO string.
+def index_lzo_file(lzo_file: BinaryIO, index_file: BinaryIO):
     """
-
-    index = StringIO()
-    index_lzo_file(StringIO(string), index)
-
-    return index.getvalue()
-
-
-def index_lzo_file(lzo_file, index_file):
-    """Index the given LZO file and write the index to the given output stream.
+    Index the given LZO stream and write the index to the given output stream
     """
-
-    for block_offset in get_lzo_blocks(lzo_file):
-        index_file.write(struct.pack(">Q", block_offset))
-
-    return index_file
+    for block in get_lzo_blocks(lzo_file):
+        index_file.write(struct.pack(">Q", block))

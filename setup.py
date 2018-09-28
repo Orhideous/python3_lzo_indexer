49ffcd893f0b7bbb7cd7565fe9e58a09df030b7e
Andriy Kushnir <a.kushnir@smartweb.com.ua>
2018-09-28 19:54:34 +0300

    Bunch of packaging stuff

--- setup.py
+++ setup.py
@@ -1,23 +1,45 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.6
+import sys
+from pathlib import Path
+from setuptools import setup, find_packages
 
-from setuptools import setup
+if not sys.version_info >= (3, 6):
+    raise RuntimeError('lzo_indexer requires at least python 3.6')
 
+readme = Path('README.rst').read_text()
 
-def read(filename):
-    return open(filename).read()
+requirements = Path('requirements.txt').read_text().splitlines()
+test_requirements = Path('requirements_dev.txt').read_text().splitlines()
 
-setup(name="lzo-indexer",
-    version="0.0.1",
-    description="Library for indexing LZO compressed files",
-    long_description=read("README.md"),
-    author="Tom Arnfeld",
-    author_email="tom@duedil.com",
-    maintainer="Tom Arnfeld",
-    maintainer_email="tom@duedil.com",
-    url="https://github.com/duedil-ltd/python-lzo-indexer",
-    download_url="https://github.com/duedil-ltd/python-lzo-indexer/archive/0.0.1.tar.gz",
-    license=read("LICENSE"),
-    packages=["lzo_indexer"],
-    scripts=["bin/lzo-indexer"],
-    test_suite="tests.test_indexer",
+setup(
+    author='Tom Arnfeld',
+    author_email='tom@duedil.com',
+    maintainer='Andriy Kushnir (Orhideous)',
+    maintainer_email='me@orhideous.name',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description='Library for indexing LZO compressed files',
+    install_requires=requirements,
+    license=Path('LICENSE').read_text(),
+    long_description=readme,
+    include_package_data=True,
+    keywords=['lzo', 'archive', 'indexing'],
+    name='python3_lzo_indexer',
+    packages=find_packages(),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/Orhideous/python3-lzo-indexer',
+    version='0.1.0',
+    zip_safe=False,
+    entry_points='''
+        [console_scripts]
+        lzo_indexer=lzo_indexer.cli:cli
+    ''',
 )
